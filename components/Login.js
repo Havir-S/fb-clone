f37097fa4117f087b1380e4d383cca3d8acc5e1f
Chris Saffron <saffronkrzysztof@gmail.com>
2023-03-09 18:58:24 +0100

    test deploy fix

--- components/Login.js
+++ components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
             style={{objectFit: 'contain'}}
             alt=''
             />
-            <h1 onClick={() => {signIn()}} className='p-5 my-2 bg-blue-500 rounded-full text-white text-center cursor-pointer'>Login to Havir's Fake Facebook</h1>
+            <h1 onClick={() => {signIn()}} className='p-5 my-2 bg-blue-500 rounded-full text-white text-center cursor-pointer'>Login to Havir Fake Facebook</h1>
             <h1 className='p-5 bg-blue-200 rounded-full text-black font-bold text-center '>Try the google login</h1>
     </div>
     )
